4eccc1bfd28f6c3d330590965e28ba4ff4eba4b6
leon30 <elber.leon@uptc.edu.co>
2023-01-16 10:16:22 -0500

    init commit

---
+++ src/main/java/com/github/leon30/usersManagment/UsersManagmentApplication.java
@@ -0,0 +1,13 @@
+package com.github.leon30.usersManagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersManagmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/leon30/usersManagment/UsersManagmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.leon30.usersManagment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersManagmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
